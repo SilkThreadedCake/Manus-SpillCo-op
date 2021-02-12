7f8961cea1d5f6770d138dd2697fb5532ba16102
Sebastian K Øie <sebagnus@gmail.com>
2021-02-12 12:07:32 +0100

    Movement
    A basic movement have been added, camera control is next.

--- Assets/Scripts/ExperimentalSeb.cs
+++ Assets/Scripts/ExperimentalSeb.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ExperimentalSeb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] [Tooltip("The speed walking causes")] [Range(0,10)] private float walkingSpeed = 1;
+
+    [Tooltip("Input to walk Horizontal")] public string horizontal ="Horizontal";
+    [Tooltip("Input to walk Vertical")] public string vertical = "Vertical";
+
+
+    private float horInput;
+    private float verInput;
+    private Rigidbody RigidB;
+
+    private void Start()
     {
+        RigidB = GetComponent<Rigidbody>();
+        CameraControlls();
+    }
+
+    private void Update()
+    {
+        Walking();
+        CameraControlls();//No use yet
+    }
+
+
+    private void Walking()
+    {
+        horInput = Input.GetAxis(horizontal); //Grabs the value of Horizontal
+        verInput = Input.GetAxis(vertical); //Grabs the value of Vertical
+
+        if (Input.GetAxis(horizontal) !=0 || (Input.GetAxis(vertical)!=0))
+        {
+            RigidB.velocity = new Vector3(horInput * walkingSpeed, 0, verInput * walkingSpeed);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CameraControlls()
     {
-        
+
     }
+
 }
