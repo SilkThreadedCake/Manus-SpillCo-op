92d075ac77a585939e2aa67f2fc0e468af3f4522
Sebastian K Øie <sebagnus@gmail.com>
2021-02-18 02:20:49 +0100

    Camera script
    The camera controls have been added, now the camera should follow the mouse whilst in game.
    
    Features a speed of camera movement, and a limiter for the range up and down.
    
    To be used on the camera itself, and has a reference for the player body if needed rotating as well.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Camera settings")]
+    [Tooltip("The speed of the camera movement")][Range(0,200)]public float mouseSensitivity = 100f; //Controlling the camera speed
+
+    [Tooltip("How high the camera can go upwards")][Range(-90, 0)] public float upViewLimit = -90; //Limiting camera movement range
+    [Tooltip("How low the camera can go downwards")] [Range(0, 90)] public float downViewLimit = 90;
+
+    [Tooltip("The parent body of the camera")]public Transform playerBody; //A refrence of the body of the player(If you want the whole body to move, not just the camera)
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;//Locks the cursor in the screen
+    }
+
+    private void Update()
+    {
+        cameraRotation();
+    }
+
+
+    private void cameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;//Grabs the MouseX input and adds speed*time
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;//Grabs the MouseY input and adds speed*time
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, upViewLimit, downViewLimit);//Limits the range of looking up & down
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKey(KeyCode.Escape)) { Cursor.lockState = CursorLockMode.None; }//Release of the cursor
+    }
+}
